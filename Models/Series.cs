8c698e8d2165fd8e61fcdad8f53f81ac254ec330
Risto-Matti F Lohtander <risto@lohtander.se>
2021-02-08 19:55:20 +0100

    Added Release date

--- Models/Series.cs
+++ Models/Series.cs
@@ -9,14 +9,15 @@ namespace MiniFigures.Models
 {
     public class Series
     {
-            [BsonId]
-            [BsonRepresentation(BsonType.ObjectId)]
-            public string Id { get; set; }
-            public string Name { get; set; }
-            public string Number { get; set; }
-            public int NumberOfFigures { get; set; }
-            public string SeriesColor { get; set; }
-            public string SeriePicture { get; set; }
-            public string DisplayName { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public int NumberOfFigures { get; set; }
+        public string SeriesColor { get; set; }
+        public string SeriePicture { get; set; }
+        public string DisplayName { get; set; }
+        public string ReleaseDate { get; set; }
     }
 }
